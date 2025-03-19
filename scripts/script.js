3f808e8d4bf09da5d5a10ef656a166a94cfc1094
EricWang559 <150875139+EricWang559@users.noreply.github.com>
2025-03-19 08:15:42 -0500

    new style

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(toggle);
 
     // Define the available theme files
-    const themes = ["styles/style.css", "styles/tnstyle.css", "styles/tnstyle2.css"];
+    const themes = ["styles/style.css", "styles/tnstyle.css", "styles/gtstyle.css"];
 
     // Retrieve current theme index from localStorage (if set) or default to 0.
     let currentThemeIndex = localStorage.getItem("themeIndex") ? parseInt(localStorage.getItem("themeIndex"), 10) : 0;
