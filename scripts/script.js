1d8f226229a691576f5a4c4cbfd723d42af6b5a1
EricWang559 <150875139+EricWang559@users.noreply.github.com>
2025-04-09 13:32:15 -0500

    why my website break

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(toggle);
 
     // Define the available theme files
-    const themes = ["styles/style1.css", "styles/gtstyle.css", "styles/astyle.css"];
+    const themes = ["styles/gtstyle.css", "styles/style1.css", "styles/astyle.css"];
 
     // Retrieve current theme index from localStorage (if set) or default to 0.
     let currentThemeIndex = localStorage.getItem("themeIndex") ? parseInt(localStorage.getItem("themeIndex"), 10) : 0;
